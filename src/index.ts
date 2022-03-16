25d1234f8ee5280713ee271941941cd027a772c6
VladimirJV <ivladimirjuarez@outlook.com>
2022-03-15 23:00:50 -0600

    init

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import fastify from 'fastify'
+import fastify, { FastifyInstance } from "fastify";
 
-const server = fastify()
+const server: FastifyInstance = fastify({});
 
-server.get('/ping', async (request, reply) => {
-  return 'pong\n'
-})
+server.get("/ping", async (request, reply) => {
+  return "pong\n";
+});
 
 server.listen(8080, (err, address) => {
   if (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
-  console.log(`Server listening at ${address}`)
-})
+  console.log(`Server listening at ${address}`);
+});
