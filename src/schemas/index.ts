0a619f38271ca0e85589c6c0dda3d8071ad13055
VladimirJV <ivladimirjuarez@outlook.com>
2022-03-18 17:14:57 -0600

    update schemas folder

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,10 +1,10 @@
 import { FastifyInstance } from "fastify";
-import PostCAtegoryBodySchemaJSON from "../../schemas/CategoryPostBodySchema.json";
+import PostCategoryBodySchemaJSON from "../../schemas/CategoryPostBodySchema.json";
 import CategorySchemaBaseJSON from "../../schemas/CategorySchema.json";
 import { JSONSchema7 } from "json-schema";
 
 
 export function addSchemas(instance: FastifyInstance) {
   instance.addSchema(CategorySchemaBaseJSON as JSONSchema7);
-  instance.addSchema(PostCAtegoryBodySchemaJSON as JSONSchema7);
+  instance.addSchema(PostCategoryBodySchemaJSON as JSONSchema7);
 }
